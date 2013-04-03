0b43367275956f373f8ea5b4ba6c11f0b35dce09
Widy <widy.agung@gmail.com>
2013-04-03 16:34:54 +0700

    INIT

---
+++ index.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Sistem Informasi Akademik | Universitas Gadjah Mada</title>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="main">
+  <div class="header">
+    <div class="header_resize">
+      <div class="logo">
+        <img src="img/logo.gif" width="3%" height="3%" />SISTEM INFORMASI AKADEMIK UNIVERSITAS GADJAH MADA
+      </div>
+      <div class="clr"></div>
+      <div class="clr"></div>
+      <!--<div class="menu_nav">
+        <ul>
+          <?php include "navigasi.php";?>
+        </ul>
+      </div>
+	  -->
+    </div>
+  </div>
+  <div class="clr"></div>
+  <div class="content">
+    <div class="content_resize">
+	  <div class="sidebar">
+        <div class="gadget">
+			<?php include "navigasi.php";?>
+		</div>
+	  </div>
+	  <div class="mainbar">
+        <div class="article">
+          test
+       </div>
+      <div class="clr"></div>
+    </div>
+  </div>
+  
+  <div class="clr"></div>
+  <div class="fbg">
+    <div class="footer">
+      <p class="lr"></p>
+      <p class="lf">&copy; 2013 UGM</p>
+      
+      <div class="clr"></div>
+    </div>
+  </div>
+
+
+
+<!-- end of main_container -->
+<div id="outer_frame">
+<div id="frame">
+	<iframe id="loader" width="570px" height="420px" frameborder="0" style="float:left;"></iframe>
+<div id="tutup" style="cursor:pointer;width:20px;height:20px;float:right;font-size:13pt;border:0px solid #000;" onclick="javascript:document.getElementById('outer_frame').style.display='none';document.getElementById('loader').src='about:blank;'"><img src="images/close.gif" /></div>
+</div>
+</div>
+
+</div>
+</div><!--end of main-->
+</body>
+</html>
---
+++ navigasi.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+<link href="css/menu.css" rel="stylesheet" type="text/css" />
+<link href="css/layout.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="container">
+<ul id="nav">
+	<!--Menu SI Monitor -->
+	<li>
+		<a href="http://localhost/mhs/">SISTEM INFORMASI MONITOR MAHASISWA KHUSUS</a>
+    </li>
+    
+    <!--Menu Evaluasi Study -->
+	<li>
+    	<a href="#">EVALUASI STUDY</a>
+        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a>
+    </li>
+    
+    <!--Menu Legalisir Ijazah-->
+	<li>
+    	<a href="#">LEGALISIR IJAZAH</a>
+        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a>
+    </li>
+    
+    <!--Menu Verifikasi Ijazah -->
+	<li>
+    	<a href="#">VERIFIKASI IJAZAH</a>
+        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a>
+    </li>
+    
+    <!--Menu Surat Izin -->
+	<li>
+    	<a href="#">SURAT IZIN</a>
+        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a>
+    </li>
+</ul>
+</body>
+
+</html>
