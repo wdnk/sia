d9d06d5d9caefb12a82f917dcbe3a9439056efca
Widy <widy.agung@gmail.com>
2013-04-04 17:18:28 +0700

    4

--- navigasi.php
+++ navigasi.php
@@ -7,9 +7,8 @@
     <!--Menu Evaluasi Study -->
 	<li>
     	<a href="#">EVALUASI STUDY</a>
-        <a href="#">Sub Menu</a>
-        <a href="#">Sub Menu</a>
-        <a href="#">Sub Menu</a>
+        <a <?php if(isset($_GET['page']) == 'evaluasi_satu'){ ?> class="active" <?php } ?> href="index.php?page=evaluasi_satu">Evaluasi I</a><br />
+        <a <?php if(isset($_GET['page']) == 'evaluasi_dua'){ ?> class="active" <?php } ?> href="index.php?page=evaluasi_dua">Evaluasi II</a>
     </li>
     
     <!--Menu Legalisir Ijazah -->
@@ -23,23 +22,16 @@
     <!--Menu Verifikasi Ijazah -->
 	<li>
     	<a href="#">VERIFIKASI IJAZAH</a>
-        <a href="#">Sub Menu</a>
-        <a href="#">Sub Menu</a>
+        <a href="#">Sub Menu</a><br>
+        <a href="#">Sub Menu</a><br>
         <a href="#">Sub Menu</a>
     </li>
     
     <!--Menu Surat Izin -->
 	<li>
     	<a href="#">SURAT IZIN</a>
-        <a 
-			<?php 
-				if($_GET['page'] == 'input_data')
-					{ 
-			?> 
-           				class="active" 
-			<?php } ?> 
-            href="index.php?page=input_data">Sub Menu</a>
-        <a href="#">Sub Menu</a>
+        <a <?php if(isset($_GET['page']) == 'input_data'){ ?> class="active" <?php } ?> href="index.php?page=input_data">Sub Menu</a><br>
+        <a href="#">Sub Menu</a><br>
         <a href="#">Sub Menu</a>
     </li>
 </ul>
