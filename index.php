ce728c97131cf1b568343dd5930f4e5b74f85b03
Widy <widy.agung@gmail.com>
2013-04-05 16:55:31 +0700

    revisi layout dan menu

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <link href="css/styles.css" rel="stylesheet" type="text/css" />
 <link href="css/menu.css" rel="stylesheet" type="text/css" />
+    <script type="text/javascript" src="js/jquery-1.3.2.min.js"></script>
+    <script type="text/javascript" src="js/simpla.jquery.configuration.js"></script>
+    <script type="text/javascript" src="js/facebox.js"></script>
+    <script type="text/javascript" src="js/jquery.wysiwyg.js"></script>
+    <!-- Internet Explorer .png-fix -->
+    <!--[if IE 6]>
+      <script type="text/javascript" src="resources/scripts/DD_belatedPNG_0.0.7a.js"></script>
+      <script type="text/javascript">
+        DD_belatedPNG.fix('.png_bg, img, li');
+      </script>
+    <![endif]-->
 </head>
 
 <body>
@@ -21,47 +32,47 @@
   <div class="content">
     <div class="content_resize">
 	  <div class="sidebar">
-        <div class="gadget">
 			<?php include "navigasi.php";?>
-		</div>
 	  </div>
-	  <div class="mainbar">
-        <div class="article">
+	  <div id="main-content">
+        <noscript> <!-- Show a notification if the user has disabled javascript -->
+          <div class="notification error png_bg">
+            <div>
+              Javascript harus aktif di browser anda
+            </div>
+          </div>
+        </noscript>
         	<?php
 				if (isset($_REQUEST['page'])){
 					$tampil=$_REQUEST['page'];
 				}
 					if(empty($tampil)){
-					  $halaman="page";
+					  $halaman="dashboard";
 					}else{
 					  $halaman=$tampil;
 					}
 					include $halaman.".php";
 			?>
-       </div>
       <div class="clr"></div>
     </div>
+      <div id="footer" class="footer">
+        <small>
+            &#169; Copyright 2013 JTETI UGM
+        </small>
+      </div><!-- End #footer -->
   </div>
-  
-  <div class="clr"></div>
-  <div class="fbg">
-    <div class="footer">
-      <p class="lr"></p>
-      <p class="lf">&copy; 2013 UGM</p>
-      
-      <div class="clr"></div>
-    </div>
-  </div>
+  <!-- end of main_container -->
 
-<!-- end of main_container -->
+
 <div id="outer_frame">
 <div id="frame">
 	<iframe id="loader" width="570px" height="420px" frameborder="0" style="float:left;"></iframe>
 <div id="tutup" style="cursor:pointer;width:20px;height:20px;float:right;font-size:13pt;border:0px solid #000;" onclick="javascript:document.getElementById('outer_frame').style.display='none';document.getElementById('loader').src='about:blank;'"><img src="images/close.gif" /></div>
 </div>
 </div>
-
 </div>
 </div><!--end of main-->
+
+
 </body>
 </html>
--- navigasi.php
+++ navigasi.php
@@ -1,37 +1,48 @@
-<ul id="nav">
-	<!--Menu SI Monitor -->
-	<li>
-		<a href="http://localhost/mhs/">SISTEM INFORMASI MONITOR MAHASISWA KHUSUS</a>
-    </li>
-    
-    <!--Menu Evaluasi Study -->
-	<li>
-    	<a href="#">EVALUASI STUDY</a>
-        <a <?php if(isset($_GET['page']) == 'evaluasi_satu'){ ?> class="active" <?php } ?> href="index.php?page=evaluasi_satu">Evaluasi I</a><br />
-        <a <?php if(isset($_GET['page']) == 'evaluasi_dua'){ ?> class="active" <?php } ?> href="index.php?page=evaluasi_dua">Evaluasi II</a>
-    </li>
-    
-    <!--Menu Legalisir Ijazah -->
-	<li>
-    	<a href="#">LEGALISIR IJAZAH</a>
-        <a href="#">Sub Menu</a><br>
-        <a href="#">Sub Menu</a><br>
-        <a href="#">Sub Menu</a>
-    </li>
-    
-    <!--Menu Verifikasi Ijazah -->
-	<li>
-    	<a href="#">VERIFIKASI IJAZAH</a>
-        <a href="#">Sub Menu</a><br>
-        <a href="#">Sub Menu</a><br>
-        <a href="#">Sub Menu</a>
-    </li>
-    
-    <!--Menu Surat Izin -->
-	<li>
-    	<a href="#">SURAT IZIN</a>
-        <a <?php if(isset($_GET['page']) == 'input_data'){ ?> class="active" <?php } ?> href="index.php?page=input_data">Sub Menu</a><br>
-        <a href="#">Sub Menu</a><br>
-        <a href="#">Sub Menu</a>
-    </li>
-</ul>
+ <div id="sidebar"><div id="sidebar-wrapper">            
+            <ul id="main-nav">  <!-- Accordion Menu -->
+                
+                <li>
+                    <a href="index.php" class="nav-top-item no-submenu">
+                        Dashboard
+                    </a>       
+                </li>
+
+                <li>
+                    <a href="http://localhost/mhs/" class="nav-top-item no-submenu">
+                        Pengurus FT
+                    </a>       
+                </li>
+
+                <li> 
+                    <a href="#" class="nav-top-item <?php if((isset($_GET['page']) == 'evaluasi_satu') || (isset($_GET['page']) == 'evaluasi_dua')){ ?> current <?php } ?>"> 
+                    Evaluasi Study
+                    </a>
+                    <ul>
+                        <li><a <?php if(isset($_GET['page']) == 'evaluasi_satu'){ ?> class="current" <?php } ?> href="index.php?page=evaluasi_satu">Evaluasi I</a></li>
+                        <li><a <?php if(isset($_GET['page']) == 'evaluasi_satu'){ ?> class="current" <?php } ?> href="index.php?page=evaluasi_dua">Evaluasi II</a></li>
+                    </ul>
+                </li>
+                
+                <li>
+                    <a href="#" class="nav-top-item">
+                        Ijazah
+                    </a>
+                    <ul>
+                        <li><a href="#">Legalisir Ijazah</a></li>
+                        <li><a href="#">Verifikasi Ijazah</a></li>
+                    </ul>
+                </li>
+                
+                <li>
+                    <a href="#" class="nav-top-item">
+                        Perizinan
+                    </a>
+                    <ul>
+                        <li><a href="#">Surat Izin I</a></li>
+                        <li><a href="#">Surat Izin II</a></li>
+                        <li><a href="#">Surat Izin III</a></li>
+                        <li><a href="#">Surat Izin IV</a></li>
+                    </ul>
+                </li>  
+            </ul> <!-- End #main-nav -->
+</div>
